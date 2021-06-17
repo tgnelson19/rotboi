403756b31b2a912da22a3258d06970aa5344b9b0
Tyler <nelson.tyler.g@gmail.com>
2021-06-17 12:07:58 -0500

    Added a simple range mechanic to the arrows

--- classes/Arrow.h
+++ classes/Arrow.h
@@ -18,7 +18,7 @@ public:
     float range;
 
 
-    Arrow() { name = "arrow"; backwards = false; damage = 3; range = 3;}
+    Arrow() { name = "arrow"; backwards = false; damage = 3; range = 2.5;}
 
     void update() {
         sprite.setRotation(angle);
